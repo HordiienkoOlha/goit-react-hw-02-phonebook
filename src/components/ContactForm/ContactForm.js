ce2786bd4785c44e89f553b7b68c71a97221d55d
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-06-07 11:53:42 +0300

    Revert "edit contact form"
    This reverts commit 3d1acdeb3f3d08a0b9db28441126fe541bb77c4f.

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -7,11 +7,10 @@ const ContactForm = () => {
         <div className={s.row}>
         <label className={s.label}>
             Name
-                <input
-                    value={name}
+            <input
             type="text"
             name="name"
-            
+            // value={name}
                         className={s.input}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
